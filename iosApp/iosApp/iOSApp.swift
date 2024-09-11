3788ed3b139912a3329008e4357aca4fd66ca2ca
frankois <dabonot.francois@gmail.com>
2024-09-11 11:15:12 +0200

    add swift code to init koin

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
+    
+    init() {
+        StartKoinKt.startApp()
+    }
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
